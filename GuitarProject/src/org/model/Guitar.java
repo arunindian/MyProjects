8d179726db67c81943a444de1abc653bdcafc151
arunindian <arun10dixit@gmail.com>
2017-03-26 17:42:22 +0530

    Initial guitar version

--- GuitarProject/src/org/model/Guitar.java
+++ GuitarProject/src/org/model/Guitar.java
@@ -1,5 +1,68 @@
 package org.model;
 
+/**
+ * 
+ * @author dixit
+ *
+ */
 public class Guitar {
+	
+	private String serialNumber;
+	private String builder;
+	private String model;
+	private String backWood;
+	private String frontWood;
+	
+	public Guitar(String serialNumber,
+			String builder,
+			String model,
+			String backWood,
+			String frontWood) {
+		this.serialNumber = serialNumber;
+		this.builder =builder;
+		this.model = model;
+		this.backWood = backWood;
+		this.frontWood = frontWood;
+	}
 
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	public String getBuilder() {
+		return builder;
+	}
+
+	public void setBuilder(String builder) {
+		this.builder = builder;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getBackWood() {
+		return backWood;
+	}
+
+	public void setBackWood(String backWood) {
+		this.backWood = backWood;
+	}
+
+	public String getFrontWood() {
+		return frontWood;
+	}
+
+	public void setFrontWood(String frontWood) {
+		this.frontWood = frontWood;
+	}
+	
 }
