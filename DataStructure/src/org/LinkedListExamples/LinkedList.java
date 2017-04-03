3668b87ae18dbebeec0db8c31992c503b64e78d5
arunindian <arun10dixit@gmail.com>
2017-04-04 00:11:55 +0530

    Adding different operation to LinkedList

--- DataStructure/src/org/LinkedListExamples/LinkedList.java
+++ DataStructure/src/org/LinkedListExamples/LinkedList.java
@@ -10,8 +10,42 @@ public class LinkedList {
 		
 		Node(int data, Node next) {
 			this.data = data;
-			this.next = null;
+			this.next = next;
 		}
+	}
+	
+	public void pushBeforeHead(int data) {
+		Node node = new Node(data,  this.head);
+		this.head = node;
+	}
+	
+	public void insertAfter(Node prevNode, int new_data) {
+		if(prevNode == null) {
+			System.out.println("Previous node cannot be null.");
+			return;
+		}
+		
+		Node node = new Node(new_data, null);
+		node.next = prevNode.next;
+		prevNode.next = node;
+	}
+	
+	public void append(int new_data) {
+		
+		Node newNode = new Node(new_data, null);
+		
+		if(head == null) {
+			head = newNode;
+			return;
+		}
+		
+		Node last = this.head;
+		
+		while(last.next != null) {
+			last = last.next;
+		}
+		
+		last.next = newNode;
 	}
 
 }
