8905dc7503752f56aeded039a9bf11430f239dc0
arunindian <arun10dixit@gmail.com>
2017-04-04 00:02:57 +0530

    Changing package structure

---
+++ DataStructure/src/org/LinkedListExamples/LinkedList.java
@@ -0,0 +1,17 @@
+package org.LinkedListExamples;
+
+public class LinkedList {
+	
+	Node head;
+	
+	class Node{
+		int data;
+		Node next;
+		
+		Node(int data, Node next) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+
+}
